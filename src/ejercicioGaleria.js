96fe1c53b59035ac54b3e189ffd52c77e7d3e440
rpmaya <35469068+rpmaya@users.noreply.github.com>
2022-09-16 12:25:17 +0200

    Update ejercicioGaleria.js

--- src/ejercicioGaleria.js
+++ src/ejercicioGaleria.js
@@ -1,11 +1,21 @@
+const max = 3;
+
 function prev() {
-    var current = document.getElementById('image').src;
-    console.log(current);
-    document.getElementById('image').src = "images/moto03.jpg";
+    var current = document.getElementById('image').getAttribute('src').substring(12,13);
+    var updated = parseInt(current) - 1;
+    if (updated === 0) {
+        updated = max;
+    }
+    var newsrc = "images/moto0" + updated + ".jpg";
+    document.getElementById('image').src = newsrc;
 }
 
 function next() {
-    var current = document.getElementById('image').src;
-    console.log(current);
-    document.getElementById('image').src = "images/moto02.jpg";
+    var current = document.getElementById('image').getAttribute('src').substring(12,13);
+    var updated = parseInt(current) + 1;
+    if (updated > max) {
+        updated = 1;
+    }
+    var newsrc = "images/moto0" + updated + ".jpg";
+    document.getElementById('image').src = newsrc;
 }
