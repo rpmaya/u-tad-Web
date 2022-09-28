885f69985330b1cbe349321ff360deaecd96f60e
rpmaya <35469068+rpmaya@users.noreply.github.com>
2022-09-28 21:18:35 +0200

    update mult langs

--- jquery/multipleLanguages/main.js
+++ jquery/multipleLanguages/main.js
@@ -1,56 +1,76 @@
 var arrLang = {
     'en': {
-        'form-header-speaker':'Registration as Speaker',
-        'form-header-company':'Registration as Company',
+        'form-header-speaker': 'Registration as Speaker',
+        'form-header-company': 'Registration as Company',
         'slogan': 'Transforming people through people!',
-        'speaker':'Speaker',
-        'company':'Company',
-        'name':'Name',
-        'surname':'Surname',
-        'password':'Password',
-        'male':'Male',
-        'female':'Female',
-        'email':'E-mail',
-        'phone':'Phone number',
-        'confirm-password':'Confirm Password',
-        'register':'Register'
+        'speaker': 'Speaker',
+        'company': 'Company',
+        'name': 'Name',
+        'surname': 'Surname',
+        'password': 'Password',
+        'male': 'Male',
+        'female': 'Female',
+        'email': 'E-mail',
+        'phone': 'Phone number',
+        'confirm-password': 'Confirm Password',
+        'register': 'Register',
+        'entrar': 'Get in'
     },
     'es': {
-        'form-header-speaker':'Registro como Ponente',
-        'form-header-company':'Registro como Empresa',
+        'form-header-speaker': 'Registro como Ponente',
+        'form-header-company': 'Registro como Empresa',
         'slogan': '¡Transformando personas a través de personas!',
-        'speaker':'Ponente',
-        'company':'Empresa',
-        'name':'Nombre',
-        'surname':'Apelido',
-        'password':'Contraseña',
-        'male':'Hombre',
-        'female':'Mujer',
-        'email':'Email',
-        'phone':'Telefóno',
-        'confirm-password':'Confirmar Contraseña',
-        'register':'Registrar'
+        'speaker': 'Ponente',
+        'company': 'Empresa',
+        'name': 'Nombre',
+        'surname': 'Apelido',
+        'password': 'Contraseña',
+        'male': 'Hombre',
+        'female': 'Mujer',
+        'email': 'Email',
+        'phone': 'Telefóno',
+        'confirm-password': 'Confirmar Contraseña',
+        'register': 'Registrar',
+        'entrar': 'Entrar'
     },
     'pt': {
-        'form-header-speaker':'Cadastro como Palestrante',
-        'form-header-company':'Cadastro como Empresa',
+        'form-header-speaker': 'Cadastro como Palestrante',
+        'form-header-company': 'Cadastro como Empresa',
         'slogan': 'Transformando pessoas atravéz de pessoas!',
-        'speaker':'Palestrante',
-        'company':'Empresa',
-        'name':'Nome',
-        'surname':'Sobrenome',
-        'password':'Senha',
-        'male':'Masculino',
-        'female':'Feminino',
-        'email':'E-mail',
-        'phone':'Número de telefone',
-        'confirm-password':'Confirmar Senha',
-        'register':'Registrar'
-    }	
+        'speaker': 'Palestrante',
+        'company': 'Empresa',
+        'name': 'Nome',
+        'surname': 'Sobrenome',
+        'password': 'Senha',
+        'male': 'Masculino',
+        'female': 'Feminino',
+        'email': 'E-mail',
+        'phone': 'Número de telefone',
+        'confirm-password': 'Confirmar Senha',
+        'register': 'Registrar',
+        'entrar': 'Entrem'
+    }
 }
 
 $(document).ready(
-    function(){
+    function () {
         //TODO
+        $(".translate").click(
+            function() {
+                var lang = $(this).attr('id');
+                $(".lang").each(
+                    function (index) {    
+                        if($(this).val() != "") {
+                           $(this).val(arrLang[lang][$(this).attr('key')])
+                        } else {
+                            if($(this).placeholder != "undefined"){
+                               $(this).prop('placeholder', arrLang[lang][$(this).attr('key')]);         
+                            }
+                            $(this).text(arrLang[lang][$(this).attr('key')]);
+                        }
+                    }
+                );
+            }
+        )
     }
 );
