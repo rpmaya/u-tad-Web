4fb6c8601397a84f76ee4674cf6f741ffe6b0ab5
Ricardo Palacios <rpmaya@gmail.com>
2023-01-13 12:35:48 +0100

    todos

--- TODOs/main.js
+++ TODOs/main.js
@@ -8,17 +8,26 @@ const addTODO = () => {
     $("#tareas").append('<li>' + $('#tarea').val() + "</li>");
 }
 
+const filterTODOs = () => {
+    $("li").show().filter(function(index) {
+        const texto = $('#buscar').val()
+        console.log(texto);
+        console.log($(this).text())
+        return $(this).text().search(texto) < 0;
+    }).hide();
+}
+
 
 $(document).ready(function () {
     $.ajax({
         //crossDomain: true;
         type: 'GET', //Valor por defecto
-        url: 'https://jsonplaceholder.typicode.com/todos', //API Pública, con Flask hicimos el API privada
+        url: 'https://jsonplaceholder.typicode.com/albums', //API Pública, con Flask hicimos el API privada
         contentType: 'application/json',
         async: true, //Valor por defecto
         //beforeSend: function() {console.log("Antes de la petición....");},
         success: function (res) { //Los datos que recibiré cuando haga la petición a la url
-           showTODOs(res.slice(0,9));
+           showTODOs(res.slice(0,19));
         }, 
         error: function (err) {
             console.log(err);
@@ -28,6 +37,12 @@ $(document).ready(function () {
     $("#add").click(
         function () {
             addTODO();
+        }
+    )
+
+    $("#search").click(
+        function () {
+            filterTODOs();
         }
     )
 
