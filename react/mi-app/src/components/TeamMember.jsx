2a75c7de2f514b59063e07cc03758c7e4db2469f
rpmaya <35469068+rpmaya@users.noreply.github.com>
2023-07-13 18:16:16 +0200

    team member

--- react/mi-app/src/components/TeamMember.jsx
+++ react/mi-app/src/components/TeamMember.jsx
@@ -4,10 +4,15 @@ function TeamMember() {
   return (
     <div className='cont-teamMember'>
       <img 
-        className='img-teamMember' 
+        className='img-member' 
         src={require('../images/Borja-Montoro.png')}
         alt='Foto de Borja'  
       />
+      <div className='cont-text'>
+        <p className='name-member'>Borja Montoro Cavero</p>
+        <p className='position-member'>Director del programa Experto en Diseño de Personajes</p>
+        <p className='description-member'>Diseñador con más de 25 años de experiencia en la industria, trabajó ...</p>
+      </div>
     </div>
   );
 }
