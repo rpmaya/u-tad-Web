ee26e372457e5f74807d9700f61cc03c3d675858
rpmaya <35469068+rpmaya@users.noreply.github.com>
2023-07-18 21:06:15 +0200

    props

--- react/mi-app/src/App.js
+++ react/mi-app/src/App.js
@@ -6,7 +6,21 @@ function App() {
     <div className='App'>
         <div className='main-container'>
           <h1>Profesores internacionales de primer nivel</h1>
-          <TeamMember />
+          <TeamMember 
+            name="Borja Montoro Cavero" 
+            position="Director del programa Experto en Diseño de Personajes" 
+            description="Diseñador con más de 25 años de experiencia en la industria, trabajó siete de ellos como..." 
+            image="Borja-Montoro"/>
+          <TeamMember 
+            name="David Míguez Blanco" 
+            position="Director del Máster en Rigging y CFX. Profesor de Rigging" 
+            description="Tras licenciarse en Historia por la Universidad de Santiago de Compostela, David cursa el Máster en ..." 
+            image="david-miguez_1"/>
+          <TeamMember 
+            name="Eduardo Fernández Carrión" 
+            position="Profesor de Probabilidad y Estadísticas, Geomertría Computacional" 
+            description="Ingeniero Informático y Matemático. Doctor en Métodos Estadísticos-Matemáticos para ..." 
+            image="eduardo-fernandez-carrion_1"/>
         </div>
     </div>
   );
--- react/mi-app/src/components/TeamMember.jsx
+++ react/mi-app/src/components/TeamMember.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import '../styles/TeamMember.css';
 
-function TeamMember() {
+function TeamMember(props) {
   return (
     <div className='cont-teamMember'>
       <img 
         className='img-member' 
-        src={require('../images/Borja-Montoro.png')}
+        src={require(`../images/${props.image}.png`)}
         alt='Foto de Borja'  
       />
       <div className='cont-text'>
-        <p className='name-member'>Borja Montoro Cavero</p>
-        <p className='position-member'>Director del programa Experto en Diseño de Personajes</p>
-        <p className='description-member'>Diseñador con más de 25 años de experiencia en la industria, trabajó ...</p>
+        <p className='name-member'>{props.name}</p>
+        <p className='position-member'>{props.position}</p>
+        <p className='description-member'>{props.description}</p>
       </div>
     </div>
   );
