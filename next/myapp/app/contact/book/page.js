aca114b44929fdc5e3a17e20e49139f154d9f197
Ricardo Palacios <rpmaya@gmail.com>
2023-10-02 19:41:01 +0200

    Routes

---
+++ next/myapp/app/contact/book/page.js
@@ -0,0 +1,10 @@
+export default function Book() {
+    return (
+        <div>
+            <h1>Reserva</h1>
+            <ul>
+                <li>Mesa</li>
+            </ul>
+        </div>
+    )
+}
---
+++ next/myapp/app/contact/page.js
@@ -0,0 +1,14 @@
+function Contact() {
+    return (
+        <div>
+            <h1>Contact</h1>
+            <ul>
+                <li>E-mail</li>
+                <li>Phone number</li>
+                <li>Social Media</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Contact
