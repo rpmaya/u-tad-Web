0fc17b056762f4acc2e13683abb8a0ad41960c79
Ricardo Palacios <rpmaya@gmail.com>
2023-10-06 21:07:50 +0200

    navbar

---
+++ next/myapp/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <h1>Navbar</h1>
+            <ul>
+                <li>
+                    <Link href="/">Home</Link>
+                </li>
+                <li>
+                    <Link href="/about">About</Link>
+                </li>
+                <li>
+                    <Link href="/contact">Contact</Link>
+                </li>
+                <li>
+                    <Link href="/contact/book">Booking</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
