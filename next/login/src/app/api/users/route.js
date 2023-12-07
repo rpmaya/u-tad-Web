657951451a5e7b41065ae0ad267632322359e862
Ricardo Palacios <rpmaya@gmail.com>
2023-12-07 08:33:46 +0000

    delete

--- next/login/src/app/api/users/route.js
+++ next/login/src/app/api/users/route.js
@@ -1,12 +1,26 @@
 import { NextResponse } from 'next/server'
-import { readFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
 
 export async function GET() {
     try{
         const users = JSON.parse(readFileSync("data/users.txt"))
-        console.log(users)
+        //console.log(users)
         return NextResponse.json({users})
     } catch(e){  
         return NextResponse.json({message: "Usuarios no existen...", status: 400})
+    }
+}
+
+export async function DELETE(request) {
+   const data = await request.json()
+   try {
+        const users = JSON.parse(readFileSync("data/users.txt"))
+        //console.log(users)
+        const usersFIlter = users.filter(user => user.email != data.email) 
+        //console.log(usersFIlter)
+        writeFileSync("data/users.txt", JSON.stringify(usersFIlter))
+        return NextResponse.json({message: "Usuario eliminado...", status: 200})
+    } catch(e){
+        console.log(e)
     }
 }
