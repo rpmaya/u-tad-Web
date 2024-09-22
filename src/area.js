fa249488a5f07f628045d45683f6fdae73c7199c
Ricardo Palacios <rpmaya@gmail.com>
2024-09-22 17:37:14 +0200

    área

--- src/area.js
+++ src/area.js
@@ -1,16 +1,3 @@
-function calcularAreaOld() {
-    const base = parseFloat(document.getElementById("base").value);
-    const altura = parseFloat(document.getElementById("altura").value);
-
-    if (isNaN(base) || isNaN(altura) || base <= 0 || altura <= 0) {
-        alert("Por favor, introduzca valores válidos para la base y la altura.");
-        return;
-    }
-
-    const area = (base * altura) / 2;
-    document.getElementById("resultado").value = area.toFixed(2);
-}
-
 function calcularArea() {
     const base = parseFloat(document.querySelector("#base").value);
     const altura = parseFloat(document.querySelector("#altura").value);
@@ -22,4 +9,17 @@ function calcularArea() {
 
     const area = (base * altura) / 2;
     document.querySelector("#resultado").value = area.toFixed(2);
+}
+
+function calcularAreaOlder() {
+    const base = parseFloat(document.getElementById("base").value);
+    const altura = parseFloat(document.getElementById("altura").value);
+
+    if (isNaN(base) || isNaN(altura) || base <= 0 || altura <= 0) {
+        alert("Por favor, introduzca valores válidos para la base y la altura.");
+        return;
+    }
+
+    const area = (base * altura) / 2;
+    document.getElementById("resultado").value = area.toFixed(2);
 }
